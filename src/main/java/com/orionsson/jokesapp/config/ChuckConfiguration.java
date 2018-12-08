fca6146e0c8846b553bbc428c9b7c258fc7c84f0
fatma arik copur <fatmaarik@gmail.com>
2018-12-08 16:04:01 -0500

    xml config of spring is added.

--- src/main/java/com/orionsson/jokesapp/config/ChuckConfiguration.java
+++ src/main/java/com/orionsson/jokesapp/config/ChuckConfiguration.java
@@ -1,12 +1,9 @@
 package com.orionsson.jokesapp.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class ChuckConfiguration {
-    @Bean
+
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
